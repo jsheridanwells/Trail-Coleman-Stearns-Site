0f45fca14ff17b3d18839a9e585584b1e4dd9237
Jeremy <jsheridanwells@gmail.com>
2018-05-13 13:27:35 -0500

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
